6f8c770ea55858120d7e36dce2b497a049a60878
MARIO LOPEZ <m-lopez22@live.com>
2020-01-28 12:23:19 -0800

    fixed js in order to display on gitpages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,164 +1,244 @@
-// global variables
-
-var resultsCorrect = 0;
-var resultsIncorrect = 0;
-var noAnswer = 0;
-var seconds = 15;
-var questionIndex = 0;
-var intervalId;
-
-//questions array in an object
-var questions = [
+// Questions
+var triviaQuestions = [
   {
-    display: "What is the name of Rocko's dog?",
-    answers: ["Sparky", "Spanner", "Punkin", "Spunky", "Sputnik"],
-    correctAnswer: "Spunky",
-    imageSrc: "assets/images/spunky.gif",
-    message: "The name of Rocko's dog is Spunky."
+    question: "What is Mac's first name?",
+    answerList: ["Peter", "Matthew", "Ronald", "Billy"],
+    answer: 2
   },
   {
-    display: "Which is NOT a team on Legends of the Hidden Temple?",
-    answers: [
-      "Green Monkeys",
-      "Purple Parrots",
-      "Orange Orangutans",
-      "Silver Snakes",
-      "Red Jaguars"
-    ],
-    correctAnswer: "Orange Orangutans",
-    imageSrc: "assets/images/Legends.gif",
-    message:
-      "'Orange Orangutans' is NOT a team on Legends of the Hidden Temple."
+    question: "What does the 'I' stand for in the Dennis system?",
+    answerList: ["Initiate breakup", "Inspire hope", "Invent danger", "Invest"],
+    answer: 1
   },
   {
-    display: "What is always the final challenge in Nickelodeon GUTS?",
-    answers: [
-      "Vertiboggan",
-      "Aggro Crag",
-      "Slam-A-Jama",
-      "Eat My Dust",
-      "Dodge It"
+    question: "What was the name of Frank's street gang from the '50s?",
+    answerList: [
+      "Yellow Jacket Boys",
+      "The Pecan Sandies",
+      "The Freight Train",
+      "Dirty Deeds"
     ],
-    correctAnswer: "Aggro Crag",
-    imageSrc: "assets/images/aggroCrag.gif",
-    message: "The fifth and final event is the Aggro Crag."
+    answer: 0
   },
-
   {
-    display: "What is Stimpy's favorite song?",
-    answers: [
-      "Everyone Wants a Log",
-      "Tickle Tickle Ichy Pickle",
-      "I LOVE MY LITTER",
-      "Three Blind Mice",
-      "HAPPY HAPPY JOY JOY!!"
+    question: "What was Dee's nickname in highschool?",
+    answerList: [
+      "Fatty Magoo",
+      "The Gangly Bird",
+      "The Aluminum Monster",
+      "The Truck"
     ],
-    correctAnswer: "HAPPY HAPPY JOY JOY!!",
-    imageSrc: "assets/images/REV.gif",
-    message: "Stimpy's favorite song is HAPPY HAPPY JOY JOY!!"
+    answer: 2
   },
-
   {
-    display:
-      "What does the monster community use as currency in 'Aaahh!!! Real Monsters'?",
-    answers: [
-      "toe nails",
-      "boogers",
-      "nuts and bolts",
-      "Hair clippings",
-      "skin flakes"
+    question:
+      "What name do Dennis, Mac, and Charlie wrestle under when they put on a show for the troops?",
+    answerList: [
+      "The Eagles of Pain",
+      "The Fowl of Action",
+      "War Birds",
+      "The Birds of War"
     ],
-    correctAnswer: "toe nails",
-    imageSrc: "assets/images/monsters.gif",
-    message: "The monsters use toe nails as a form of currency."
+    answer: 3
+  },
+  {
+    question: "What was Frank's nickname from his business days?",
+    answerList: ["Trash Man", "The Warthog", "Froggy", "Littleman"],
+    answer: 1
+  },
+  {
+    question:
+      "Which of the following are the correct ingredients for fight milk?",
+    answerList: [
+      "Milk, yoghurt and rum",
+      "Milk, eggs and vodka",
+      "Milk, cat food and tequila",
+      "Milk, ketchup and whiskey"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is Mac and Charlie's shared annual Christmas tradition?",
+    answerList: [
+      "Throwing rocks at trains.",
+      "Breaking into houses together to steal presents.",
+      "Attending midnight mass.",
+      "Delivering stolen presents to the orphanage."
+    ],
+    answer: 0
+  },
+  {
+    question:
+      "What's the flip cup drinking game that Paddy's Pub has been banned from (twice)?",
+    answerList: [
+      "Flip Cup Championships",
+      "Flipsylvania",
+      "Flipadelphia",
+      "Fliperty Bell"
+    ],
+    answer: 2
+  },
+  {
+    question: "'Kitten Mittens! You'll be.....",
+    answerList: ["Smitten", "Sittin' happy", "Bitten", "Kitten me"],
+    answer: 0
   }
 ];
 
-function nextQuestion() {
-  if (questionIndex < questions.length - 1) {
-    questionIndex++;
-    timerReset();
-    $("#questions").css("visibility", "visible");
-    $("#afterClick").css("visibility", "hidden");
-    displayAnswers();
-  } else {
-    clearInterval(intervalId);
-    alert("end game");
-    $("#afterClick").css("visibility", "hidden");
-    $("#results").css("visibility", "visible");
-  }
-}
+console.log(triviaQuestions);
 
-function startTimer() {
-  intervalId = setInterval(count, 1000);
-  $("#startButton").css("visibility", "hidden");
-  $("#questions").css("visibility", "visible");
-}
+var gifArray = [
+  "question1",
+  "question2",
+  "question3",
+  "question4",
+  "question5",
+  "question6",
+  "question7",
+  "question8",
+  "question9",
+  "question10"
+];
+var currentQuestion;
+var correctAnswer;
+var incorrectAnswer;
+var unanswered;
+var seconds;
+var time;
+var answered;
+var userSelect;
+var messages = {
+  correct: "Yes, that's right!",
+  incorrect: "Sorry, that's not right.",
+  endTime: "Out of time!",
+  finished: "Alright! Let's see how well you did."
+};
 
-function count() {
-  seconds--;
-
-  if (seconds === 0) {
-    checkAnswer(false);
-  }
-
-  $("#questionTimer").html("<p>" + seconds + "</p");
-}
-
-function timerReset() {
-  seconds = 15;
-  $("#questionTimer").html("<p>" + seconds + "</p");
-}
-
-function resetQuiz() {
-  questionIndex = 0;
-  resultsCorrect = 0;
-  resultsIncorrect = 0;
-  $("#results").css("visibility", "hidden");
-  displayAnswers();
-  startTimer();
-  timerReset();
-}
-
-//loop through answers array
-function displayAnswers() {
-  var q = questions[questionIndex];
-  $("#triviaQuestion").html(q.display);
-  for (var i = 0; i < q.answers.length; i++) {
-    var id = i + 1;
-    $("#answer" + id).html(q.answers[i]);
-  }
-  console.log(q.display);
-}
-
-//on Click function, tallys score and checks right/wrong/no answer
-function checkAnswer(isClick, userAnswer) {
-  var msg = "";
-  if (!isClick) {
-    msg = "Sorry, time's up! ";
-    noAnswer++;
-    $("#noAnswer").text("Not answered: " + noAnswer);
-  } else if (userAnswer === questions[questionIndex].correctAnswer) {
-    msg = "Correct!! ";
-    resultsCorrect++;
-    $("#resultsCorrect").text("Correct answers: " + resultsCorrect);
-  } else {
-    msg = "Wrong!! ";
-    resultsIncorrect++;
-    $("#resultsIncorrect").text("Incorrect answers: " + resultsIncorrect);
-  }
-
-  $("#afterClick").css("visibility", "visible");
-  $("#questions").css("visibility", "hidden");
-  $("#gif").attr("src", questions[questionIndex].imageSrc);
-  $("#confirm").text(msg + questions[questionIndex].message);
-  setTimeout(nextQuestion, 5000);
-}
-
-//continuation of on Click funtion, passes user anwser so "this" will work
-$(".answerSection").on("click", ".answer", function() {
-  var userAnswer = $(this).text();
-  checkAnswer(true, userAnswer);
+$("#startBtn").on("click", function() {
+  $(this).hide();
+  newGame();
 });
 
-displayAnswers();
+$("#startOverBtn").on("click", function() {
+  $(this).hide();
+  newGame();
+});
+
+function newGame() {
+  $("#finalMessage").empty();
+  $("#correctAnswers").empty();
+  $("#incorrectAnswers").empty();
+  $("#unanswered").empty();
+  currentQuestion = 0;
+  correctAnswer = 0;
+  incorrectAnswer = 0;
+  unanswered = 0;
+  newQuestion();
+}
+
+function newQuestion() {
+  $("#message").empty();
+  $("#correctedAnswer").empty();
+  $("#gif").empty();
+  answered = true;
+
+  //sets up new questions & answerList
+  $("#currentQuestion").html(
+    "Question #" + (currentQuestion + 1) + "/" + triviaQuestions.length
+  );
+  $(".question").html(
+    "<h2>" + triviaQuestions[currentQuestion].question + "</h2>"
+  );
+  for (var i = 0; i < 4; i++) {
+    var choices = $("<div>");
+    choices.text(triviaQuestions[currentQuestion].answerList[i]);
+    choices.attr({ "data-index": i });
+    choices.addClass("thisChoice");
+    $(".answerList").append(choices);
+  }
+  countdown();
+  //clicking an answer will pause the time and setup answerPage
+  $(".thisChoice").on("click", function() {
+    userSelect = $(this).data("index");
+    clearInterval(time);
+    answerPage();
+  });
+}
+
+function countdown() {
+  seconds = 15;
+  $("#timeLeft").html("<h3>Time Remaining: " + seconds + "</h3>");
+  answered = true;
+  //sets timer to go down
+  time = setInterval(showCountdown, 1000);
+}
+
+function showCountdown() {
+  seconds--;
+  $("#timeLeft").html("<h3>Time Remaining: " + seconds + "</h3>");
+  if (seconds < 1) {
+    clearInterval(time);
+    answered = false;
+    answerPage();
+  }
+}
+
+function answerPage() {
+  $("#currentQuestion").empty();
+  $(".thisChoice").empty(); //Clears question page
+  $(".question").empty();
+
+  var rightAnswerText =
+    triviaQuestions[currentQuestion].answerList[
+      triviaQuestions[currentQuestion].answer
+    ];
+  var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+  $("#gif").html(
+    '<img src = "assets/images/' +
+      gifArray[currentQuestion] +
+      '.gif" width = "400px">'
+  );
+
+  //checks to see correct, incorrect, or unanswered
+  if (userSelect == rightAnswerIndex && answered == true) {
+    correctAnswer++;
+    $("#message").html(messages.correct);
+  } else if (userSelect != rightAnswerIndex && answered == true) {
+    incorrectAnswer++;
+    $("#message").html(messages.incorrect);
+    $("#correctedAnswer").html("The correct answer was: " + rightAnswerText);
+  } else {
+    unanswered++;
+    $("#message").html(messages.endTime);
+    $("#correctedAnswer").html("The correct answer was: " + rightAnswerText);
+    answered = true;
+  }
+
+  if (currentQuestion == triviaQuestions.length - 1) {
+    setTimeout(scoreboard, 4500);
+  } else {
+    currentQuestion++;
+    setTimeout(newQuestion, 4500);
+  }
+}
+
+function scoreboard() {
+  $("#timeLeft").empty();
+  $("#message").empty();
+  $("#correctedAnswer").empty();
+  $("#gif").empty();
+
+  $("#finalMessage").html(messages.finished);
+  $("#correctAnswers").html("Correct Answers: " + correctAnswer);
+  $("#incorrectAnswers").html("Incorrect Answers: " + incorrectAnswer);
+  $("#unanswered").html("Unanswered: " + unanswered);
+
+  var audioElement = document.createElement("audio");
+  audioElement.setAttribute("src", "assets/images/wildCard.mp3");
+  $("#startOverBtn").addClass("reset");
+  $("#startOverBtn").show();
+  $("#startOverBtn").html("Start Over?");
+  $("#startOverBtn").on("click", function() {
+    audioElement.play();
+  });
+}
